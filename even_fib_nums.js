6cc6d60939ad664c71b46524bc601baa6c53b9a0
Aukai Tirrell <mtirrell@hawaii.edu>
2016-11-12 14:13:35 -1000

    finished first part of fib nums

--- even_fib_nums.js
+++ even_fib_nums.js
@@ -7,11 +7,34 @@
  */
 function _sumFibs( maxFibValue ) {
   var sum = 0;
+  var first = 0;
+  var second = 1;
+  var fibArray = [];
+
+  while (first + second <= maxFibValue) {
+    var nextNum = first + second;
+    console.log('in while lopp: nextNum', nextNum)
+    fibArray.push(nextNum);
+    console.log('array is ', fibArray)
+    first = second;
+    second = nextNum;
+  }
+ for (var i = 0; i <= fibArray.length; i++) {
+   if (fibArray[i] % 2 === 0) {
+
+     sum += fibArray[i];
+   }
+ }
+
+
+
 
   // do your work here
 
   return sum;
 }
+console.log(_sumFibs(10));
+
 
 // bonus round
 function _highestFibonacciNumber (maxFibValue){
@@ -33,3 +56,4 @@ module.exports = {
   sumFibs : _sumFibs,
   highestFibonacciNumber : _highestFibonacciNumber
 };
+
